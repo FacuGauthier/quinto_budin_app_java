d0237f8f0d18aba0a4945007635f5c6590cad0c3
FacuGauthier <facundosgauthier@gmail.com>
2026-05-10 20:50:13 -0300

    add entity DetallesCompra

---
+++ src/main/java/org/example/models/DetallesCompra.java
@@ -0,0 +1,68 @@
+package org.example.models;
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "detalles_compra")
+public class DetallesCompra {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "compra_id", nullable = false)
+    private Compra compra;
+
+    @ManyToOne
+    @JoinColumn(name = "ingrediente_id", nullable = false)
+    private Ingrediente ingrediente;
+
+    @Column(name = "cantidad_comprada", nullable = false)
+    private Integer cantidadComprada;
+
+    @Column(name = "precio_unitario", nullable = false, precision = 10, scale = 2)
+    private BigDecimal precioUnitario;
+
+    public DetallesCompra() {
+    }
+    public DetallesCompra(Compra compra, Ingrediente ingrediente, Integer cantidadComprada, BigDecimal precioUnitario) {
+        this.compra = compra;
+        this.ingrediente = ingrediente;
+        this.cantidadComprada = cantidadComprada;
+        this.precioUnitario = precioUnitario;
+    }
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public Compra getCompra() {
+        return compra;
+    }
+    public void setCompra(Compra compra) {
+        this.compra = compra;
+    }
+    public Ingrediente getIngrediente() {
+        return ingrediente;
+    }
+    public void setIngrediente(Ingrediente ingrediente) {
+        this.ingrediente = ingrediente;
+    }
+    public Integer getCantidadComprada() {
+        return cantidadComprada;
+    }
+    public void setCantidadComprada(Integer cantidadComprada) {
+        this.cantidadComprada = cantidadComprada;
+    }
+    public BigDecimal getPrecioUnitario() {
+        return precioUnitario;
+    }
+    public void setPrecioUnitario(BigDecimal precioUnitario) {
+        this.precioUnitario = precioUnitario;
+    }
+}
