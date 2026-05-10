9bf5675f51c62af65b6391c06925f511e9d22fb5
FacuGauthier <facundosgauthier@gmail.com>
2026-05-10 20:29:57 -0300

    add entity Ingrediente

---
+++ src/main/java/org/example/models/Ingrediente.java
@@ -0,0 +1,89 @@
+package org.example.models;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "ingredientes")
+public class Ingrediente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nombre;
+
+    @Column(nullable = false)
+    private String marca;
+
+    @Column(name = "stock_actual", nullable = false)
+    private Integer stockActual;
+
+    @Column(nullable = false)
+    private boolean activo;
+
+    @Column(name = "fecha_creacion", nullable = false)
+    private LocalDateTime fechaCreacion;
+
+    @Column(name = "fecha_creacion", nullable = false)
+    private LocalDateTime fechaModificacion;
+
+    public Ingrediente() {
+    }
+    public Ingrediente(String nombre, String marca, Integer stockActual, boolean activo) {
+        this.nombre = nombre;
+        this.marca = marca;
+        this.stockActual = stockActual;
+        this.activo = activo;
+    }
+
+    @PrePersist
+    protected void onCreate() {
+        this.fechaCreacion = LocalDateTime.now();
+        this.fechaModificacion = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        this.fechaModificacion = LocalDateTime.now();
+    }
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public String getMarca() {
+        return marca;
+    }
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+    public Integer getStockActual() {
+        return stockActual;
+    }
+    public void setStockActual(Integer stockActual) {
+        this.stockActual = stockActual;
+    }
+    public boolean isActivo() {
+        return activo;
+    }
+    public void setActivo(boolean activo) {
+        this.activo = activo;
+    }
+    public LocalDateTime getFechaCreacion() {
+        return fechaCreacion;
+    }
+    public LocalDateTime getFechaModificacion() {
+        return fechaModificacion;
+    }
+}
