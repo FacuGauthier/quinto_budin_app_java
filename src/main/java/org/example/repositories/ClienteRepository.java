f9e4ef0e83e3f88c72e72c1e5b7b5064a21e7379
FacuGauthier <facundosgauthier@gmail.com>
2026-05-13 14:41:09 -0300

    add ClienteRepository

---
+++ src/main/java/org/example/repositories/ClienteRepository.java
@@ -0,0 +1,10 @@
+package org.example.repositories;
+
+import org.example.models.Cliente;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+    List<Cliente> findByActivoTrue();
+}
