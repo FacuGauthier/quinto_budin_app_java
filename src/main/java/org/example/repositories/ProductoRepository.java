90e962ba79870c8fd6d5f4a345b0171803c4017b
FacuGauthier <facundosgauthier@gmail.com>
2026-05-13 14:42:04 -0300

    add ProductoRepository

---
+++ src/main/java/org/example/repositories/ProductoRepository.java
@@ -0,0 +1,10 @@
+package org.example.repositories;
+
+import org.example.models.Producto;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ProductoRepository extends JpaRepository<Producto, Long> {
+    List<Producto> findByActivoTrue();
+}
