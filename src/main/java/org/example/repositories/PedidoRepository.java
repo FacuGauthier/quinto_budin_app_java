0c6f10dfab95bf403368ec5fdc384fce78dadf6a
FacuGauthier <facundosgauthier@gmail.com>
2026-05-13 14:46:01 -0300

    add PedidoRepository

---
+++ src/main/java/org/example/repositories/PedidoRepository.java
@@ -0,0 +1,12 @@
+package org.example.repositories;
+
+import org.example.models.Pedido;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Date;
+import java.util.List;
+
+public interface PedidoRepository extends JpaRepository<Pedido, Long> {
+    List<Pedido> findByEstado(String estado);
+    List<Pedido> findByFechaReciboBeetween(Date inicio, Date fin);
+}
