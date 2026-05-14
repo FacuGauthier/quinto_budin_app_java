120e9a6690a7fa8e79736f035101f1d92d847314
FacuGauthier <facundosgauthier@gmail.com>
2026-05-13 23:49:03 -0300

    add ProductoIngredienteRepository

---
+++ src/main/java/org/example/repositories/ProductoIngredienteRepository.java
@@ -0,0 +1,7 @@
+package org.example.repositories;
+
+import org.example.models.ProductoIngrediente;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductoIngredienteRepository extends JpaRepository<ProductoIngrediente, Long> {
+}
