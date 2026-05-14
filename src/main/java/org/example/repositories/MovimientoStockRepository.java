2a4da04ea2d576d01aefc84247f488eab0ed04a9
FacuGauthier <facundosgauthier@gmail.com>
2026-05-14 19:33:39 -0300

    fix type parameter in 'findByTipo()' in MovimientoStockRepository

--- src/main/java/org/example/repositories/MovimientoStockRepository.java
+++ src/main/java/org/example/repositories/MovimientoStockRepository.java
@@ -1,11 +1,12 @@
 package org.example.repositories;
 
 import org.example.models.MovimientoStock;
+import org.example.models.Tipo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface MovimientoStockRepository extends JpaRepository<MovimientoStock, Long> {
     List<MovimientoStock> findByIngredienteId(Long ingredienteId);
-    List<MovimientoStock> findByTipo(String tipo);
+    List<MovimientoStock> findByTipo(Tipo tipo);
 }
