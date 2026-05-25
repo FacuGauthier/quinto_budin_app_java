c5dc2f56c2fd67c0e5c01922e1716eda06af06c6
FacuGauthier <facundosgauthier@gmail.com>
2026-05-25 19:49:49 -0300

    add ExceptionHandler of DataIntegrityViolationException on GlobalExceptionHandler

--- src/main/java/org/example/exceptions/GlobalExceptionHandler.java
+++ src/main/java/org/example/exceptions/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package org.example.exceptions;
 
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -8,6 +9,11 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class GlobalExceptionHandler {
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException e) {
+        return ResponseEntity.badRequest().body(e.getMessage());
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<String> handleDataIntegrityViolationException(DataIntegrityViolationException e) {
         return ResponseEntity.badRequest().body(e.getMessage());
     }
 }
